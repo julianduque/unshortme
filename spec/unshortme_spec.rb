ca92576d4c185b1e1718742ae935453d80003643
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-07-12 22:59:39 -0500

    Adding more test cases

--- spec/unshortme_spec.rb
+++ spec/unshortme_spec.rb
@@ -4,8 +4,23 @@ describe 'Unshortme' do
 
   it "should not return the unshorted url if the url isn't short" do
     unshorted = Unshortme.unshort("the url")
-    unshorted.should_not == nil
     unshorted.should match /the url/
+  end
+
+  it "should not return the unshorted url if the api response isn't a valid json" do
+    json = "<html><body></body></html>"
+    json.stub(:code).and_return(200)
+    RestClient.stub(:get).and_return(json)
+    unshorted = Unshortme.unshort("http://bit.ly/lathw6")
+    unshorted.should == "http://bit.ly/lathw6"
+  end
+
+  it "should not return the unshorted url if the api response code isn't 200" do
+    json = "<html><body></body></html>"
+    json.stub(:code).and_return(500)
+    RestClient.stub(:get).and_return(json)
+    unshorted = Unshortme.unshort("http://bit.ly/lathw6")
+    unshorted.should == "http://bit.ly/lathw6"
   end
 
   it 'should return the unshorted url' do
@@ -19,7 +34,6 @@ describe 'Unshortme' do
     json.stub(:code).and_return(200)
     RestClient.stub(:get).and_return(json)
     unshorted = Unshortme.unshort("http://bit.ly/lathw6")
-    unshorted.should_not == nil
     unshorted.should == 'http://get2java.blogspot.com/2011/07/whats-up-in-java-17.html'
   end
 end
