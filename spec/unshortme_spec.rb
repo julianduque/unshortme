cd42feab2f722859bbff662573d17d225e3eda27
Guillermo Iguaran <guilleiguaran@gmail.com>
2011-07-12 22:48:45 -0500

    Use stubs to avoid connecting to internet

--- spec/unshortme_spec.rb
+++ spec/unshortme_spec.rb
@@ -1,15 +1,24 @@
 require 'spec_helper'
 
 describe 'Unshortme' do
-  
-  it 'should not return the unshorted url' do
+
+  it "should not return the unshorted url if the url isn't short" do
     unshorted = Unshortme.unshort("the url")
     unshorted.should_not == nil
     unshorted.should match /the url/
   end
 
   it 'should return the unshorted url' do
-    unshorted = Unshortme.unshort("http://bit.ly/lathw5")
+    json = <<-JSON
+      {
+        "requestedURL":"http:\/\/bit.ly\/lathw6",
+        "success":"true",
+        "resolvedURL":"http:\/\/get2java.blogspot.com\/2011\/07\/whats-up-in-java-17.html"
+      }
+    JSON
+    json.stub(:code).and_return(200)
+    RestClient.stub(:get).and_return(json)
+    unshorted = Unshortme.unshort("http://bit.ly/lathw6")
     unshorted.should_not == nil
     unshorted.should == 'http://get2java.blogspot.com/2011/07/whats-up-in-java-17.html'
   end
